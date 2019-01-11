13e79ccd5aa88b6a7d01866e3c1efee091eb403e
Peter Kreppein <pmkreppein@gmail.com>
2019-01-11 06:24:10 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+	return document.querySelector('[name="name"]').value;
+}
+
+function addNewElementAsLi() {
+	let x = retrieveEmployeeInformation()
+	document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${x}</li>` )
+}
+
+function addNewLiOnClick() {
+	let submit = document.querySelector('input[type="submit"]')
+	submit.addEventListener('click', function(e) {
+		addNewElementAsLi()
+		document.querySelector('input').value = ''
+	})
+}
+
+function clearEmployeeListOnLinkClick() {
+	let clearClick = document.querySelector('a')
+	let ul = document.querySelectorAll('.employee-list')
+	clearClick.addEventListener('click', function(event) {
+		ul.forEach(e => e.innerHTML = '');
+	})
+} 
